221a6f345ff27b43db9932864457090a722a2f4a
Patrick Sodré <sodre@sodre.co>
2017-11-17 13:45:55 -0800

    Add TLS CA Server back

--- csd/scripts/control.sh
+++ csd/scripts/control.sh
@@ -456,11 +456,52 @@ nifi() {
     nifi_run "$@"
 }
 
+hadoop_xml_to_json() {
+  xsltproc aux/hadoop2element-value.xslt server.hadoop_xml > server.xml
+  xsltproc aux/xml2json.xslt server.xml | jq '
+    .configuration |
+    .port=(.port| tonumber) |
+    .days=(.days | tonumber) |
+    .keySize=(.keySize | tonumber) |
+    .reorderDn=(.reorderDn == "true")' > server.json
+}
+
+ca_server_init() {
+    # NiFi 1.4.0 was compiled with 1.8.0
+    locate_java8_home $1
+
+    # Simulate NIFI_TOOLKIT_HOME
+    NIFI_TOOLKIT_HOME=$(pwd)
+    [ -e lib ] || ln -s ${CDH_NIFI_TOOLKIT_HOME}/lib .
+
+    hadoop_xml_to_json
+}
+
+ca_server_run() {
+    LIBS="${NIFI_TOOLKIT_HOME}/lib/*"
+
+    CLASSPATH=".:${LIBS}"
+
+    export JAVA_HOME="$JAVA_HOME"
+    export NIFI_TOOLKIT_HOME="$NIFI_TOOLKIT_HOME"
+
+    umask 0077
+    exec "${JAVA}" -cp "${CLASSPATH}" ${JAVA_OPTS:--Xms12m -Xmx24m} ${CSD_JAVA_OPTS} org.apache.nifi.toolkit.tls.TlsToolkitMain server --configJsonIn server.json -F
+}
+
+ca_server() {
+    init "$1"
+    run "$@"
+}
+
 case "$1" in
     nifi-run|nifi-stop)
         nifi ${1//nifi-/} "${@:2}"
         ;;
+    ca-server-run)
+        ca-server ${1//ca-server-/} "${@:2}"
+        ;;
     *)
-        echo "Usage control.sh {nifi-run|nifi-stop}"
+        echo "Usage control.sh {nifi-run|nifi-stop|ca-server-run}"
         ;;
 esac
